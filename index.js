9f89cd673d4d8e6f71ce4cfe7915ecbe7fe92279
sreypheak <phorn.sreypheak@institute.pse.ngo>
2025-01-24 13:51:32 +0700

    add all

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const display = document.getElementById("display")
+const display = document.getElementById("display");
 
-function appendTodisplay(input){
-    display.value += input;
+function appendTodisplay(input) {
+  display.value += input;
 }
 
-function clearDisplay(){
-    display.value = "";
+function clearDisplay() {
+  display.value = "";
 }
 
-function calsulate(){
-    try{
-        display.value = eval(display.value);
-    }
-    catch(error){
-        display.value = "Error"
-    }
-    
+function calsulate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
 }
+function Solve(input) {
+  display.value /= input;
+}
+
